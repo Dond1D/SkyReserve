ef59ee53f1a6eaabd94331653f6e9e9cd1cb7e5b
Dond1D <donineti@hotmail.com>
2024-03-01 16:50:30 +0100

    Updated Important Files

--- html/service.php
+++ html/service.php
@@ -21,41 +21,76 @@ if ($conn->connect_error) {
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
-    <h1 class="text-center mt-5">Service Table</h1>
-<table class="styled-table">
-    <thead>
-        <tr>
-            <th>Name</th>
-            <th>Description</th>
-            <th>Date Created</th>
-            <th>Category Name </th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr class="active-row"></tr>
-        <?php
-      $sql = "SELECT s.name,s.description,s.date_created,c.name  as CategoryName
-      FROM services as s inner join category c on s.category_id = c.id;";
-      $result = $conn->query($sql);
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark ">
+        <div class="container-fluid ">
+            <div class="container  d-flex justify-content-between">
+                <a class="navbar-brand" href="./index.html">
+                    <img src="../images/dd-logo-letter-monogram-slash-with-modern-vector-27833344-removebg-preview.png"
+                        alt="Logo" width="100px" height="100px">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#collapsibleNavbar">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="d-flex">
+                    <div class="collapse navbar-collapse " id="collapsibleNavbar">
+                        <ul class="navbar-nav ">
+                            <!-- <li class="nav-item">
+                                <a class="nav-link" href="./index.html">Home</a>
+                            </li> -->
+                            
+                            <li class="nav-item">
+                                <a class="nav-link" href="./admin.php">Contact</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="./index.html">Log out</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
 
-      if ($result->num_rows > 0) {
-        while($row = $result->fetch_assoc()) {
+            </div>
+        </div>
+    </nav>
+
+
+
+
+    <h1 class="text-center mt-5">Service Table</h1>
+        <table class="styled-table">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Description</th>
+                    <th>Date Created</th>
+                    <th>Category Name </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="active-row"></tr>
+                <?php
+            $sql = "SELECT s.name,s.description,s.date_created,c.name  as CategoryName
+            FROM services as s inner join category c on s.category_id = c.id;";
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0) {
+                while($row = $result->fetch_assoc()) {
+                    
+                echo "<tr>";
+                echo "<td>" . $row["name"] . "</td>";
+                echo "<td>" . $row["description"] . "</td>";
+                echo "<td>" . $row["date_created"] . "</td>";
+                echo "<td>" . $row["CategoryName"] . "</td>";
+                echo "</tr>";
+                }
+                
+            } else {
+            echo "<tr><td colspan='4'>No results found</td></tr>";
+            }
             
-          echo "<tr>";
-          echo "<td>" . $row["name"] . "</td>";
-          echo "<td>" . $row["description"] . "</td>";
-          echo "<td>" . $row["date_created"] . "</td>";
-          echo "<td>" . $row["CategoryName"] . "</td>";
-          echo "</tr>";
-        }
-        
-       } else {
-      echo "<tr><td colspan='4'>No results found</td></tr>";
-       }
-      
-      $conn->close();
-    ?>
-    </tbody>
-</table>
+            $conn->close();
+            ?>
+            </tbody>
+        </table>
 </body>
 </html>
--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
+function validateSignup(event){
+    event.preventDefault()
+     let fname = document.getElementById("fname").value
+     let lname = document.getElementById("lname").value
+     let email = document.getElementById("email").value
+     let pwd = document.getElementById("pwd").value
+     let gender = document.getElementById("radioMale").value
 
+     const data = {
+         emri : fname,
+         mbiemri : lname,
+         emaili : email,
+         passwordi : pwd,
+         gjinia : gender
+     }
+     console.log(data)
+ }
+
+
+ 
